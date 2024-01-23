c1a135ffc6af113871b51ace7e84704912df3f91
Daniel Romero <danieljromero305@gmail.com>
2024-01-23 15:59:16 -0500

    Created CSS and JS files, updated website

---
+++ scripts.js
@@ -0,0 +1,39 @@
+function dropText(id, dropID) {
+    //console.log(id);
+    //console.log(dropID);
+    var text = document.getElementById(id).innerHTML;
+    /*var startText = text.substring(0, 4);
+    console.log(text);
+    console.log(startText);*/
+    //console.log(text.substring(5, text.length))
+    //console.log(text);
+
+    document.getElementById(dropID).classList.toggle("show");
+    if(document.getElementById(dropID).classList.contains('show')){
+        document.getElementById(id).innerHTML = "&or;";
+    }else{
+        document.getElementById(id).innerHTML = "&gt; ";
+    }
+    //document.getElementById("schoolProjectsButton").innerHTML = "&lt;School Projects";
+
+    
+}
+
+window.onclick = function (event) {
+    if (!event.target.matches('.dropbtn')) {
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+
+                var arrows = document.getElementsByClassName("dropdownArrow")
+                for(var i = 0; i < arrows.length; i++){
+                    arrows[i].innerHTML = "&gt;";
+                }
+            }
+        }
+        
+    }
+}
